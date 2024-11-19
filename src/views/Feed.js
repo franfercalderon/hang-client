5e089e02246acc4fbfae998e1ff3a213909e2662
Franco Fernandez <fran.fercalderon@gmail.com>
2024-11-19 15:15:42 -0300

    update login2

--- src/views/Feed.js
+++ src/views/Feed.js
@@ -1,4 +1,11 @@
+import { useContext } from "react";
+import { AppContext } from '../../context/AppContext'
+
 export default function Feed () {
+
+    //CONTEXT
+    const { authToken } = useContext( AppContext )
+    console.log(authToken);
     return(
         <p>FEED LOCO</p>
     )
