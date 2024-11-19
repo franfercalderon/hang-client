eb016f19457a840d41354848f5854685124b54a7
Franco Fernandez <fran.fercalderon@gmail.com>
2024-11-19 17:06:12 -0300

    update login2

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,7 +1,9 @@
+import { useEffect } from 'react'
 import Swal from 'sweetalert2'
 
 export default function Alert({ title, text, icon, confirmBtn, showDenyBtn, denyBtn }) {
-    return(
+
+    useEffect(() => {
         Swal
             .fire({
                 title: title,
@@ -18,5 +20,24 @@ export default function Alert({ title, text, icon, confirmBtn, showDenyBtn, deny
                     denyButton: 'deny-btn btn order1',
                 }
             })
-    )
+    }, [ title, text, icon, confirmBtn, showDenyBtn, denyBtn ] )
+    return null 
+    // return(
+    //     Swal
+    //         .fire({
+    //             title: title,
+    //             text: text,
+    //             icon: icon,
+    //             confirmButtonText: confirmBtn,
+    //             showDenyButton: showDenyBtn,
+    //             denyButtonText: denyBtn,
+    //             buttonsStyling: false,
+    //             customClass: {
+    //                 popup: 'hang-alert-container',
+    //                 icon: 'alert-icon',
+    //                 confirmButton: 'confirm-btn btn order2',
+    //                 denyButton: 'deny-btn btn order1',
+    //             }
+    //         })
+    // )
 }
