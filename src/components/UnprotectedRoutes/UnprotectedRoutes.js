65e29897a14ff41aaea5dcfa1d72ddac641599cc
Franco Fernandez <fran.fercalderon@gmail.com>
2024-12-23 19:27:44 -0300

    testing

--- src/components/UnprotectedRoutes/UnprotectedRoutes.js
+++ src/components/UnprotectedRoutes/UnprotectedRoutes.js
@@ -1,21 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from '../../context/AppContext'
-import Loader from "../Loader/Loader";
 
 export default function UnprotectedRoute({ children }) {
 
     //CONTEXT
     const { globalUser } = useContext( AppContext )
 
-    // if( tokenLoading ){
-    //     return(
-    //         <Loader/>
-    //     )
-    // }
-
     if ( globalUser ) {
-        console.log('a ver');
         return <Navigate to="/" replace/>
     }
     
