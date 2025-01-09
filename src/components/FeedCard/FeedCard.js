27ae11cd39b7ec308bbd01471140f022b10f576f
Franco Fernandez <fran.fercalderon@gmail.com>
2025-01-09 15:06:31 -0300

    working on even cards to display street view

--- src/components/FeedCard/FeedCard.js
+++ src/components/FeedCard/FeedCard.js
@@ -1,7 +1,15 @@
 import { faChevronRight, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useCallback, useEffect, useState } from "react";
+import useMaps from "../../hooks/useMaps";
 
 export default function FeedCard({ title, descritpion, starts, ends, location, action, ctaText, erase, userName, userImg, border }){
+
+    //STATE
+    const [ streetViewUrl, setStreetViewUrl ] = useState( null )
+
+    //HOOKS 
+    const { getLocationImage } = useMaps()
 
     //FUNCTIONS
     const converTimestampToString = ( timestamp ) => {
@@ -17,11 +25,26 @@ export default function FeedCard({ title, descritpion, starts, ends, location, a
             const ampm = hours >= 12 ? 'pm' : 'am'
             hours = hours % 12 || 12
             const formattedMinutes = minutes.toString().padStart( 2, '0' )
-    
             return `${ hours }:${ formattedMinutes } ${ ampm }`
         }
-
     }
+
+    const handleStreetViewUrl = useCallback( async ( coordinates ) => {
+        try {
+            const url = await getLocationImage( coordinates )
+            console.log(url);
+            setStreetViewUrl( url )
+        } catch ( error ) {
+            console.log( error );
+        }
+    }, [ getLocationImage ])
+
+    //EFFECTS
+    useEffect(() => {
+        if( location.coordinates ){
+            handleStreetViewUrl( location.coordinates )
+        }
+    }, [ location, handleStreetViewUrl ])
 
     return(
         <div className={` feed-card rounded cta-card ${ !ctaText ? 'pointer' : ''} ${ border ? 'borders' : '' }`} onClick={ !ctaText ? action : undefined }>
