1a3b6c8581d89bf5cd27027cc07ee898cc9b8395
Franco Fernandez <fran.fercalderon@gmail.com>
2025-01-20 13:09:03 -0300

    solving maps url issues

--- src/components/LocationInput/LocationInput.js
+++ src/components/LocationInput/LocationInput.js
@@ -1,25 +1,21 @@
-import { GoogleMap, LoadScript, Autocomplete } from '@react-google-maps/api'
+import { Autocomplete } from '@react-google-maps/api'
 import { useState, useRef, useEffect } from 'react'
-const loadLibraries = [ 'places' ]
 
 export default function LocationInput({ handleChange }) {
 
     //STATE
     const [ inputValue, setInputValue ] = useState( '' )
     const [ dropdownVisible, setDropdownVisible ] = useState( false )
-    const [ isLoaded, setIsLoaded ] = useState( false )
     
     //REFS
     const autocompleteRef = useRef( null )
-
-    // //VARIABLES
-    // console.log(process.env.REACT_APP_MAPS_LOAD_LIBRARIES);
 
     //FUNCTIONS
     const handlePlaceSelected = () => {
         const place = autocompleteRef.current.getPlace()
         if ( place ){
             handleChange( place )
+            console.log(place);
             setInputValue( place.formatted_address || '' )
             setDropdownVisible( false )
         }
@@ -46,35 +42,6 @@ export default function LocationInput({ handleChange }) {
     
     
     return(
-        // <>
-        //     <p>Location</p>
-        //     <LoadScript
-        //         googleMapsApiKey={ process.env.REACT_APP_MAPS_API_KEY }
-        //         libraries={  loadLibraries }
-        //         onLoad={() => setIsLoaded(true)}
-
-        //     >   
-        //     {
-        //         isLoaded &&
-        //         <Autocomplete
-        //             onLoad={( autocomplete ) => ( autocompleteRef.current = autocomplete )}
-        //             onPlaceChanged={ handlePlaceSelected }
-        //         >
-        //             <input 
-        //                 type="text" 
-        //                 placeholder=''
-        //                 autoComplete="off" 
-        //                 value={ inputValue } 
-        //                 className={`location-input ${ dropdownVisible ? 'active' : '' }`}
-        //                 onFocus={ handleInputFocus }
-        //                 id={ 'location-input' } 
-        //                 name={ 'location-input' } 
-        //                 onChange={ handleInputChange }
-        //             />
-        //         </Autocomplete>
-        //     }
-        //     </LoadScript>
-        // </>
         <>
             <p>Location</p>
             <Autocomplete
