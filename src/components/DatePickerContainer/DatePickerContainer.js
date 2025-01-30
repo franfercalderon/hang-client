5ab2b408fdafe0684c084c6451fbe4c29b69fef1
Franco Fernandez <fran.fercalderon@gmail.com>
2025-01-30 20:22:50 -0300

    upgrading UX

--- src/components/DatePickerContainer/DatePickerContainer.js
+++ src/components/DatePickerContainer/DatePickerContainer.js
@@ -6,7 +6,15 @@ export default function DatePickerContainer({ selectedDate , setSelectedDate }){
     return(
         <div className='mt-1'>
             <label htmlFor={ 'datepicker' }>{ 'Date' }</label>
-            <DatePicker selected={ selectedDate } readOnly={ true } onChange={ ( date ) => setSelectedDate( date ) } name={'datepicker'} className="main-input datepicker pointer" dateFormat={"MMMM do, yyyy"}/>
+            <DatePicker 
+                selected={selectedDate} 
+                onChange={(date) => setSelectedDate(date)} 
+                name={'datepicker'} 
+                className="main-input datepicker pointer" 
+                dateFormat={"MMMM do, yyyy"}
+                customInput={<div className="custom-datepicker">{ selectedDate ? selectedDate.toLocaleDateString() : ""}</div>}
+            />
+            {/* <DatePicker selected={ selectedDate } readOnly={ true } onChange={ ( date ) => setSelectedDate( date ) } name={'datepicker'} className="main-input datepicker pointer" dateFormat={"MMMM do, yyyy"}/> */}
         </div> 
     )
 }
